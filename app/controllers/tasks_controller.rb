b60bf81c0fda1865ba9e24a2eab6c23cbd67fde1
SergheMazur <serhio1007@gmail.com>
2018-08-07 13:56:12 -0400

    finish all challenges

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+
+  def index         # GET /tasks
+    @tasks = Task.all
+  end
+
+  def show          # GET /tasks/:id
+  end
+
+  def new           # GET /tasks/new
+    @task = Task.new
+  end
+
+  def create        # POST /tasks
+    @task = Task.new(title: task_params["title"],details: task_params["details"])
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def edit          # GET /tasks/:id/edit
+  end
+
+  def update        # PATCH /tasks/:id
+    @task.update(task_params)
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy       # DELETE /tasks/:id
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  task = Task.new(title: Faker::Dessert.topping, details: Faker::Robin.quote)
+  task.save
+end
